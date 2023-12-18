0f163c9c43e8ef4e41667a17e9da3e0fc4433ee1
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2023-12-18 16:18:07 +0000

    Add welcome text and name input field

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+print("Welcome to Hogwarts School of Witchcraft and Wizardry! Now that you turned eleven years old it is time for you to create your own history and leave behind a legacy in Hogwarts once you have completed your magical studies. Now before you embark on your journey becoming a wizard*ress we have to sort out an important detail: which house you will devote yourself to. There is the house of Gryffindor, known for its bravery and determination; along with the house of Ravenclaw, represented by its intelligence and wisdom; or the house of Slytherin characterized by its ambition and leadership. Fourth and not to forget there is the house of Hufflepuff which brings forth hard-working, loyal and honest individuals. Please, have a seat on this ceremony chair. The Sorting Hat will know where you belong...")
+
+your_name = input("Young wizard*ress, what is your name? ")
+print("Hello " + your_name + "! Now, let me see what house will bring forth the best in you...")

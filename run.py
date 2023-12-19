becebb8c1c3ba80c01017bd80e4161c78023dd5f
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2023-12-19 13:05:19 +0000

    Add varying print statements to enter_hogwarts if else statement

--- run.py
+++ run.py
@@ -35,53 +35,46 @@ print("""
 
 """)
 
+
 def enter_hogwarts():
     """
     Ask user if they want to enter Hogwarts (= start the game)
     """
-    enter = input("Enter Hogwarts (yes/no)")
+    enter = input("Enter Hogwarts (yes/no) ")
     if enter == "yes":
         welcome_greeting()
-    elif:
-        enter == "no":
-            print("You just missed your chance to become a great wizard*ess.")
+    elif enter == "no":
+        print("\nYou just missed your chance to become a great wizard*ess...\n")
+        enter_hogwarts()
     else:
-        print("Nothing happens")
+        print("\nPlease enter yes or no\n")
+        enter_hogwarts()
 
-    else:
-        enter != "yes":
-            raise RuntimeError('Please type yes to enter Hogwarts')
 
 def welcome_greeting():
     """
     Print welcome text and ask for users name
     """
-    print("Welcome to Hogwarts School of Witchcraft and Wizardry!\n\nNow that you have come here it is time for you\nto create your own history and leave behind a legacy in Hogwarts\nonce you have completed your magical studies.\nNow before you embark on your journey becoming a wizard*ress\nwe have to sort out an important detail: which house you will devote yourself to.\n\nThere is the house of Gryffindor, known for its bravery and determination;\nalong with the house of Ravenclaw, represented by its intelligence and wisdom;\nor the house of Slytherin characterized by its ambition and leadership.\nFourth and not to forget there is the house of Hufflepuff which brings forth hard-working, loyal and honest magicians.\n\nPlease, have a seat on this ceremony chair. The Sorting Hat will know where you belong...\n")
+    print("Welcome to Hogwarts School of Witchcraft and Wizardry!\n\nNow that you have come here it is time for you\nto create your own history and leave behind a legacy in Hogwarts\nonce you have completed your magical studies.\nNow before you embark on your journey becoming a wizard*ess\nwe have to sort out an important detail: which house you will devote yourself to.\n\nThere is the house of Gryffindor, known for its bravery and determination;\nalong with the house of Ravenclaw, represented by its intelligence and wisdom;\nor the house of Slytherin characterized by its ambition and leadership.\nFourth and not to forget there is the house of Hufflepuff which brings forth hard-working, loyal and honest magicians.\n\nPlease, have a seat on this ceremony chair. The Sorting Hat will know where you belong...\n")
 
-    your_name = input("\x1B[3mYoung wizard*ress, what is your name? \x1B[0m")
+    your_name = input("\x1B[3mYoung wizard*ess, what is your name? \x1B[0m")
     print("\x1B[3m\nHello " + your_name + "!\n\nLet me see what house will bring forth the best in you...")
     
-    global confirm_start
     confirm_start = input("\nNow, this is peculiar! ...the decision is more complex than I thought.\nI will need to get to know you better to find the right house for you...\nare you ready to dive deeper with me?\x1B[0m (type \x1B[3myes\x1B[0m to continue) ").lower()
-
-def start_questions():
-    """
-    Triggers first question
-    """
     if confirm_start == "yes":
-      #  first_question()
+        #first_question()
         print("You entered yes!")
-    else:
-        while confirm_start != "yes":
-          raise RuntimeError('Please enter yes to continue')
-
-
+    else: 
+        if confirm_start != "yes":
+            raise RuntimeError('Please enter yes to continue')
     
+
 def main():
     """
     Calling all functions
     """
     enter_hogwarts()
-    start_questions()
+
 
 main()
+
